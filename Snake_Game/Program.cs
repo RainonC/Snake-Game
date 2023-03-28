856e9bac83db4635c532343c7dc400b5bdd69a98
RainonC <118743359+RainonC@users.noreply.github.com>
2023-03-28 11:36:42 +0300

    Add files via upload
    вторая, но не рабочая версия

---
+++ Snake_Game/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+
+            // Отрисовка точек			
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            int score = 0; // переменная для хранения очков
+            while (true)
+            {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    score += 10; // добавляем 10 очков за съеденную еду
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+            }
+            // Записываем очки в файл и считываем их
+            List<int> scores = new List<int>();
+            if (File.Exists("scores.txt"))
+            {
+                // Читаем очки из файла, если файл существует
+                string[] lines = File.ReadAllLines("scores.txt");
+                foreach (string line in lines)
+                {
+                    if (int.TryParse(line, out int scoreFromFile))
+                    {
+                        scores.Add(scoreFromFile);
+                    }
+                }
+            }
+            // Добавляем текущие очки в список и сортируем его по убыванию
+            scores.Add(score);
+            scores.Sort();
+            scores.Reverse();
+
+            // Сохраняем отсортированные очки в файл
+            using (StreamWriter sw = new StreamWriter("scores.txt"))
+            {
+                foreach (int s in scores)
+                {
+                    sw.WriteLine(s);
+                }
+            }
+
+            WriteGameOver();
+            Console.ReadLine();
+        }
+
+        static void WriteGameOver()
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+            WriteText("Game Over!", xOffset + 1, yOffset++);
+            yOffset++;
+            WriteText("Made by Rainon Kaska", xOffset + 2, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+        }
+
+        static void WriteText(String text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ Snake_Game/ScoreManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Snake
+{
+    class ScoreManager
+    {
+        private int score;
+        private List<int> scoresList;
+
+        public ScoreManager()
+        {
+            score = 0;
+            scoresList = new List<int>();
+            LoadScores();
+        }
+
+        public void AddScore(int points)
+        {
+            score += points;
+        }
+
+        public int GetScore()
+        {
+            return score;
+        }
+
+        public void SaveScore()
+        {
+            scoresList.Add(score);
+            scoresList.Sort();
+            scoresList.Reverse();
+            using (StreamWriter writer = new StreamWriter("scores.txt"))
+            {
+                foreach (int score in scoresList)
+                {
+                    writer.WriteLine(score);
+                }
+            }
+        }
+
+        private void LoadScores()
+        {
+            if (File.Exists("scores.txt"))
+            {
+                using (StreamReader reader = new StreamReader("scores.txt"))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (int.TryParse(line, out int score))
+                        {
+                            scoresList.Add(score);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
